15396e619d06817e5d5204a28be617ddfc1a98e8
Jurjan Bootsma <100533753+JurjanBootsma@users.noreply.github.com>
2024-03-02 20:41:46 +0100

    New main code

--- main.cc
+++ main.cc
@@ -1,28 +1,49 @@
+// Exercise 10: Main code
+// Author: Jurjan Bootsma
+
 #include <iostream>
-#include <fstream>
-#include "Calorimeter.hh"
-#include "CaloReader.hh"
-#include "CaloReaderException.hh"
-#include "CaloReco.hh"
+#include "Calorimeter/Calorimeter.hh"
+#include "Calorimeter/CaloReader.hh"
+#include "Calorimeter/CaloReco.hh"
+using namespace std ;
 
-/*
-Example of using the calorimeter classes.
-*/
+// Main code to test the calorimeter, its reading and its clustering
 int main() {
-    const char* file = "data/calo.dat";
-    CaloReader cr(file);
-    cr.readEvent() ;
-    cr.calo().dumpEvent() ;
-    CaloReco rec(cr.calo()) ;
-    rec.findClusters() ;
-    rec.dumpClusters() ;
+    // Test if the calorimeter code was working on itself
+    Calorimeter cm(20,10) ;
+    cout << "x-position: " << cm.position().x() << endl ;
+    cout << "Energy of cell [1,1]: " << cm.grid().cell(1,1)->energy() << endl << endl ;
+    // It all works correctly
 
-    cr.readEvent() ;
-    cr.calo().dumpEvent() ;
-    CaloReco rec2(cr.calo()) ;
-    rec2.findClusters() ;
-    rec2.dumpClusters() ;
-    
+    CaloReader* reader = 0; // Define CaloReader pointer
 
-    return 0 ;
+    // Try exception when a wrong file is tried to be read
+    try {
+        reader = new CaloReader("wrong.dat") ;
+    } catch (CaloReaderException cle) {
+        cout << cle << endl ; // Gives indeed the error message
+    }
+
+    // Now try it for the correct file
+    try {
+        reader = new CaloReader("data/calo.dat") ;
+    } catch (CaloReaderException cle) {
+        cout << cle << endl ;
+    }
+
+    CaloReco rec(reader->calo()) ; // Create CaloReco for clustering
+
+    // Show all the IDs in the grid, energies and clusters
+    int event_num = 1 ; // Start at first event
+    while(reader->readEvent()) {
+        cout << "Event " << event_num << ":" << endl ;
+        reader->calo().dumpReadoutMap() ; // Print IDs
+        reader->calo().dumpEvent() ; // Print energies
+        rec.findClusters() ;
+        reader->calo().dumpClusters() ; // Print clusters
+        event_num++ ;
+    }
+
+    delete reader ; // Delete the reader
+
 }
